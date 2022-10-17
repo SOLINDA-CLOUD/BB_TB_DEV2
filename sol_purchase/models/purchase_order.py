60c5a66948bdb066c778f6f60aabecc8ef4ceb78
Luthfi Achmaddin Nizar <Luthfinizar97@gmail.com>
2022-10-17 23:49:01 +0700

    color and size in purchase

--- sol_purchase/models/purchase_order.py
+++ sol_purchase/models/purchase_order.py
@@ -39,6 +39,8 @@ class PurchaseOrderLine(models.Model):
   fabric_po = fields.Many2one('data.fabric.lining', string='Fabric')
   lining_po = fields.Many2one('data.fabric.lining', string='Lining')
   color = fields.Many2many('product.template.attribute.value', string="Size and Color")
+  colour = fields.Char('Color',compute="_onchange_color_size")
+  size = fields.Char('Size',compute="_onchange_color_size")
   label = fields.Many2one(comodel_name='label.comp', string='Label')
   prod_comm = fields.Html(string='Production Comment')
 
@@ -50,10 +52,26 @@ class PurchaseOrderLine(models.Model):
         self.image = self.product_id.image_1920
       return image
 
-  @api.onchange('product_id')
+  @api.depends('product_id')
   def _onchange_color_size(self):
-    if self.product_id:
-      color = ''
-      if self.product_id.product_template_variant_value_ids:
-        self.color = self.product_id.product_template_variant_value_ids
-      return color
+    for i in self:
+      c,s = '',''
+      if i.product_id.product_template_variant_value_ids:
+        i.color = i.product_id.product_template_variant_value_ids
+        list_size = ['SIZE:','SIZES:','UKURAN:']
+        list_color = ['COLOR:','COLOUR:','COLOURS:','COLORS:','WARNA:','CORAK:']
+        for v in i.product_id.product_template_variant_value_ids:
+          if any(v.display_name.upper().startswith(word) for word in list_color):
+            c += '('+v.name+')'
+          elif any(v.display_name.upper().startswith(word) for word in list_size):
+            s += '('+v.name+')'
+          else:
+            c += ''
+            s += ''
+      else:
+        c = ''
+        s = ''
+      i.colour = c
+      i.size = s
+
+            
