73dec73a4a29bbb951463c4f5a6a8e6f28dc0b24
inicoding <irfannurrahmat@outlook.com>
2022-10-18 20:36:57 +0700

    updt manifest

--- sol_purchase/__manifest__.py
+++ sol_purchase/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'purchase_request', 'purchase', 'stock', 'purchase_stock','sol_bb_product'],
+    'depends': ['base', 'purchase_request', 'purchase', 'stock', 'purchase_stock','sol_bb_product', 'product'],
 
     # always loaded
     'data': [
