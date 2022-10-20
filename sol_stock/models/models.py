ecdfbd05d17b7e05bf5c7a736848ffed9698f109
inicoding <irfannurrahmat@outlook.com>
2022-10-20 15:09:58 +0700

    check error

--- sol_stock/models/models.py
+++ sol_stock/models/models.py
@@ -5,3 +5,8 @@ class Picking(models.Model):
   _inherit = 'stock.picking'
 
   origin_pr = fields.Many2one('purchase.request', 'Source Document', index=True, states={'done': [('readonly', True)], 'cancel': [('readonly', True)]}, help="Reference of the document")
+
+class StockPickingType(models.Model):
+  _inherit = 'stock.picking.type'
+
+  mandatory_source = fields.Boolean('Required Source Documents', default=False)
